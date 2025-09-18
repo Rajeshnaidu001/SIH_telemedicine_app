871ef9599925bc430c99a75fde761f1ef17233cb
RajeshChinta <chinta.lakshmirajesh@gmail.com>
2025-09-18 15:01:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sih_telemedicine_app/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.example.sih_telemedicine_app.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Home page");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
